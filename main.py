06e188198b6b01b241f23e327947c3ae5372bc4c
milosnowcat <ramiro.alvarez@rahcode.com>
2023-08-19 18:45:48 -0600

    TODO

---
+++ main.py
@@ -0,0 +1,6 @@
+# TODO hacer un programa con ventana
+# donde el usuario pueda insertar un elnace de youtube
+# y pueda elegir dos opciones: Peek y Download.
+# Si se elige Peek se mostrará el título del video,
+# el canal, y la miniatura.
+# Si se elige Download se descargará el video.
